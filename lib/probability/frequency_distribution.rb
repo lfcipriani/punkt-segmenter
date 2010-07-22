928434f271a08c472e08e6ebd987a84e0c69d027
Luis Cipriani <lfcipriani@gmail.com>
2010-07-22 14:34:15 -0300

    adding more features to frequency distribution module

--- lib/probability/frequency_distribution.rb
+++ lib/probability/frequency_distribution.rb
@@ -2,10 +2,15 @@ class FrequencyDistribution < Hash
 
   attr_reader :N
 
-  alias_method :B, :size
+  alias_method :B      , :size
+  alias_method :samples, :keys
 
-  #TODO: add sorted keys list, sorted value list, to_s
   def initialize
+    super
+    clear
+  end
+
+  def clear
     super
     @N = 0
     @cache = {}
@@ -21,6 +26,33 @@ class FrequencyDistribution < Hash
     @cache = {}
   end
 
+  def keys
+    result = @cache[:ordered_by_frequency_desc] || order_by_frequency_desc
+    result.map { |item| item[0] }
+  end
+
+  def values
+    result = @cache[:ordered_by_frequency_desc] || order_by_frequency_desc
+    result.map { |item| item[1] }
+  end
+
+  def items
+    @cache[:ordered_by_frequency_desc] || order_by_frequency_desc
+  end
+
+  def each(&block)
+    items = @cache[:ordered_by_frequency_desc] || order_by_frequency_desc
+    items.each { |item| yield(item[0], item[1]) }
+  end
+
+  def each_key(&block)
+    keys.each { |item| yield(item) }
+  end
+
+  def each_value(&block)
+    values.each { |value| yield(value) }
+  end
+
   def <<(sample)
     self.inc(sample)
   end
@@ -28,6 +60,19 @@ class FrequencyDistribution < Hash
   def inc(sample, count = 1)
     return if count == 0
     self[sample] = self[sample] + count
+  end
+  
+  def delete(sample, &block)
+    result = super
+    if result
+      @cache = {}
+      @N -= result
+    end
+    result
+  end
+
+  def delete_if(&block)
+    raise "Not implemented for Frequency Distributions"
   end
 
   def frequency_of(sample)
@@ -51,9 +96,24 @@ class FrequencyDistribution < Hash
   end
 
   def merge(other_frequency_distribution)
+    temp = self.dup
+    other_frequency_distribution.each do |sample, value|
+      temp.inc(sample, value)
+    end
+    return temp
+  end
+
+  def merge!(other_frequency_distribution)
     other_frequency_distribution.each do |sample, value|
       self.inc(sample, value)
     end
+    self
+  end
+
+private
+  
+  def order_by_frequency_desc
+    @cache[:ordered_by_frequency_desc] = self.to_a.sort {|x,y| y[1] <=> x[1] }
   end
 
 end
--- test/probability/frequency_distribution_test.rb
+++ test/probability/frequency_distribution_test.rb
@@ -4,7 +4,7 @@ require "probability/frequency_distribution"
 class FrequencyDistributionTest < Test::Unit::TestCase
 
   def setup
-    @words = %w(one two three one one three two one two)
+    @words = %w(two one three one one three two one two)
     @freq_dist = FrequencyDistribution.new
   end
 
@@ -53,12 +53,60 @@ class FrequencyDistributionTest < Test::Unit::TestCase
 
   def test_merge_frequency_distribution
     @words.each { |word| @freq_dist << word }
-    @freq_dist.merge(@freq_dist)
+    @new_freq = @freq_dist.merge(@freq_dist)
 
-    assert_equal @freq_dist["one"]  , 8
-    assert_equal @freq_dist["two"]  , 6
-    assert_equal @freq_dist["three"], 4
-    assert_equal @freq_dist.N       , 18
+    assert_equal @new_freq["one"]  , 8
+    assert_equal @new_freq["two"]  , 6
+    assert_equal @new_freq["three"], 4
+    assert_equal @new_freq.N       , 18
+
+    assert_equal @freq_dist.merge!(@new_freq).N, 27
+  end
+
+  def test_get_keys_ordered_by_frequency_desc
+    @words.each { |word| @freq_dist << word }
+    
+    assert_equal @freq_dist.keys.first, "one"
+    assert_equal @freq_dist.keys[1]   , "two"
+    assert_equal @freq_dist.keys.last , "three"
+ end
+
+  def test_get_values_ordered_by_frequency_desc
+    @words.each { |word| @freq_dist << word }
+    
+    assert_equal @freq_dist.values.first, 4
+    assert_equal @freq_dist.values[1]   , 3
+    assert_equal @freq_dist.values.last , 2
+  end
+
+  def test_iterators_must_order_by_frequency_desc
+    @words.each { |word| @freq_dist << word }
+    ordered = []
+    @freq_dist.each do |sample, value| 
+      ordered << [sample, value]
+    end
+    assert_equal ordered, @freq_dist.items
+
+    ordered = []
+    @freq_dist.each_key do |keys| 
+      ordered << keys
+    end
+    assert_equal ordered, @freq_dist.keys
+
+    ordered = []
+    @freq_dist.each_value do |value| 
+      ordered << value
+    end
+    assert_equal ordered, @freq_dist.values
+  end
+
+  def test_removing_samples
+    @words.each { |word| @freq_dist << word }
+    
+    assert_equal @freq_dist.delete("one"), 4
+    assert_equal @freq_dist.N            , 5
+
+    assert_raise { @freq_dist.delete_if { |sample, value| value == 2 } }
   end
 
   def test_features_with_empty_distribution
