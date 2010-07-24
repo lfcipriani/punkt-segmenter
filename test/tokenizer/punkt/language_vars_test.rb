9b76e0d6c8e2d1de34696f850198f1b2357a6d03
Luis Cipriani <lfcipriani@gmail.com>
2010-07-23 22:53:45 -0300

    minor fixes on language vars + tests

---
+++ test/tokenizer/punkt/language_vars_test.rb
@@ -0,0 +1,22 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../test_helper')
+require "tokenizer/punkt/language_vars"
+
+class PunktLanguageVarsTest < Test::Unit::TestCase
+
+  def setup
+    @lang_var = Punkt::LanguageVars.new
+    @sample = %Q{For example, the word "abbreviation" can itself be represented by the abbreviation abbr., abbrv. or abbrev.}
+  end
+
+  def test_word_tokenize
+    tokens = @lang_var.word_tokenize(@sample)
+
+    assert_equal 20  , tokens.size
+    assert_equal true, tokens.include?("abbr.")
+    assert_equal true, tokens.include?("\"")
+    assert_equal true, tokens.include?(",")
+    assert_equal true, tokens.include?("itself")
+  end
+
+end
+
