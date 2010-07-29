60ec34ff74449f253806f5d823fcc54297a3a0b3
Luis Cipriani <lfcipriani@gmail.com>
2010-07-29 17:41:41 -0300

    punkt sentence tokenizer partial implementation, almost finishing

--- lib/nlp/tokenizer/punkt/sentence_tokenizer.rb
+++ lib/nlp/tokenizer/punkt/sentence_tokenizer.rb
@@ -45,6 +45,43 @@ module Punkt
       tokens = annotate_second_pass(tokens)
       return tokens
     end
+    
+    def annotate_second_pass(tokens)
+      pair_each(tokens) do |tok1, tok2|
+        next unless tok2
+        
+        next unless tok1.ends_with_period?
+        
+        token            = tok1.token
+        type             = tok1.type_without_period
+        next_token       = tok2.token
+        next_type        = tok2.type_without_sentence_period
+        token_is_initial = tok1.is_initial?
+        
+        if @parameters.collocations.include?([type, next_type])
+          tok1.sentence_break = true
+          tok1.abbr           = true
+        end
+        
+        if (tok1.abbr || tok1.ellipsis) && !token_is_initial
+          is_sentence_starter = orthographic_heuristic(tok2)
+          #...
+        end
+        
+      end
+    end
+    
+    def orthographic_heuristic(aug_token)
+      return false if [';', ',', ':', '.', '!', '?'].include?(aug_token.token)
+      
+      orthographic_context = @parameters.orthographic_context[aug_token.type_without_sentence_period]
+      
+      return true if aug_token.first_upper? && (orthographic_context & Punkt::ORTHO_LC) || !(orthographic_context & Punkt::ORTHO_MID_UC)
+      
+      return true if aug_token.first_lower? && (orthographic_context & Punkt::ORTHO_UC) || !(orthographic_context & Punkt::ORTHO_BEG_LC)
+      
+      return :unknown
+    end
   
   end
 end
