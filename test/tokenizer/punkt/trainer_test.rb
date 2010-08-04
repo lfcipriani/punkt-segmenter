55107b3caefd8f70ac2d40ce385a9288512dbbb8
Luis Cipriani <lfcipriani@gmail.com>
2010-08-03 23:27:22 -0300

    moving tests

--- test/tokenizer/punkt/trainer_test.rb
+++ test/tokenizer/punkt/trainer_test.rb
@@ -1,22 +1,11 @@
 # encoding: utf-8
 require File.expand_path(File.dirname(__FILE__) + '/../../test_helper')
 require "nlp/tokenizer/punkt"
-require "benchmark"
 
 class PunktTrainerTest < Test::Unit::TestCase
 
   def test_train_text
-    text = File.read(File.expand_path(File.dirname(__FILE__) + "/../../data/abbr.txt"))
-    result = 0
-    puts Benchmark.measure {
-      trainer = Punkt::Trainer.new()
-      trainer.train(text)
-      tokenizer = Punkt::SentenceTokenizer.new(trainer.get_parameters)
-      result = tokenizer.tokenize(text)
-    }
-    
-    require "ap"
-    ap result
+    assert true
   end
   
 end
--- test/tokenizer/punkt_test.rb
+++ test/tokenizer/punkt_test.rb
@@ -3,12 +3,15 @@ require "nlp/tokenizer/punkt"
 
 class PunktTest < Test::Unit::TestCase
 
-  def setup
-    #punkt = Punkt.new
-  end
-
-  def test_simple
-    assert true
+  def test_number_of_sentences
+    text = File.read(File.expand_path(File.dirname(__FILE__) + "/../data/abbr.txt"))
+    
+    trainer = Punkt::Trainer.new()
+    trainer.train(text)
+    tokenizer = Punkt::SentenceTokenizer.new(trainer.get_parameters)
+    result = tokenizer.tokenize(text)
+        
+    assert_equal 45, result.size
   end
 
 end
