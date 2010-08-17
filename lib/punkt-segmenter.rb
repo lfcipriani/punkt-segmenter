9b7b6427535a0e7ff0774c6ff890cbd45828a6a3
Luis Cipriani <lfcipriani@gmail.com>
2010-08-17 16:06:21 -0300

    restricting gem to ruby 1.9

--- lib/punkt-segmenter.rb
+++ lib/punkt-segmenter.rb
@@ -1,9 +1,13 @@
-$:.unshift(File.dirname(__FILE__)) unless $:.include?(File.dirname(__FILE__))
+if RUBY_VERSION >= "1.9"
+  $:.unshift(File.dirname(__FILE__)) unless $:.include?(File.dirname(__FILE__))
 
-# Dependencies
-require "unicode_utils"
-require "set"
+  # Dependencies
+  require "unicode_utils"
+  require "set"
 
-# Lib requires
-require "punkt-segmenter/frequency_distribution"
-require "punkt-segmenter/punkt"
+  # Lib requires
+  require "punkt-segmenter/frequency_distribution"
+  require "punkt-segmenter/punkt"
+else
+  raise "This gem requires Ruby 1.9 or superior."
+end
