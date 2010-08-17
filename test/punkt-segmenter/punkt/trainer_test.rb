82a23cf7bb23b94d6041e36401582328c7735e03
Luis Cipriani <lfcipriani@gmail.com>
2010-08-16 23:23:45 -0300

    better trainer test

--- test/punkt-segmenter/punkt/trainer_test.rb
+++ test/punkt-segmenter/punkt/trainer_test.rb
@@ -3,8 +3,30 @@ require File.expand_path(File.dirname(__FILE__) + '/../../test_helper')
 
 class PunktTrainerTest < Test::Unit::TestCase
 
-  def test_train_text
-    assert true
+  def test_train_basic_portuguese_text_with_error
+    not_so_good_trainning_data = File.read(File.expand_path(File.dirname(__FILE__) + "/../../data/canudos.txt"))
+    
+    trainer = Punkt::Trainer.new()
+    trainer.train(not_so_good_trainning_data)
+    
+    parameters = trainer.parameters
+
+    # 'gol' is a word, not an abbreviation, the trainning text isn't good enough
+    assert parameters.abbreviation_types.include?("gol") 
+  end
+  
+  def test_improve_trainning_of_portuguese_text
+    not_so_good_trainning_data = File.read(File.expand_path(File.dirname(__FILE__) + "/../../data/canudos.txt"))
+    text_with_gol_as_a_word    = File.read(File.expand_path(File.dirname(__FILE__) + "/../../data/gripe.txt"))
+    
+    trainer = Punkt::Trainer.new()
+    trainer.train(not_so_good_trainning_data)
+    trainer.train(text_with_gol_as_a_word)
+    
+    parameters = trainer.parameters
+
+    # 'gol' is a word now, the trainning was better
+    assert !parameters.abbreviation_types.include?("gol")
   end
   
 end
